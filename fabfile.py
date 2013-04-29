3a6e9fb9788a1c07aa2588f483f24741304083df
Tony Angerilli <tony@angerilli.ca>
2013-04-28 18:32:00 -0700

    Updated for new deployment

--- fabfile.py
+++ fabfile.py
@@ -11,17 +11,27 @@ def live():
     env.hosts = ['servername']
     env.user = 'username'
 
+def prereqs():
+    sudo("apt-get install python-virtualenv nginx")
+
+def setup_nginx():
+    put("drop2mail.nginx", "/tmp")
+    sudo("mv /tmp/drop2mail.nginx /etc/nginx/sites-available/drop2mail")
+    sudo("ln -sf /etc/nginx/sites-available/drop2mail /etc/nginx/sites-enabled/drop2mail")
+    sudo("/etc/init.d/nginx restart")
+
 def deploy_scripts():
+    prereqs()
     local("find site -name '*.pyc' -delete")
-    run("mkdir -p drop2mail")
-    put("site/*", "drop2mail/")
-    with cd("drop2mail"):
+    run("mkdir -p sites/drop2mail")
+    put("site/*", "sites/drop2mail/")
+    with cd("sites/drop2mail"):
         run("virtualenv --no-site-packages env")
         run("env/bin/pip install -r requirements.txt")
 
 def update():
     with settings(warn_only=True):
-        run("killall gunicorn")
+        run("kill `ps ax | grep gunicorn | grep drop2mail | awk '{ print $1 }'`")
     deploy_scripts()
-    with cd("drop2mail"):
+    with cd("sites/drop2mail"):
         run("env/bin/gunicorn -D -b 127.0.0.1:4337 drop2mail:app", pty=False)
